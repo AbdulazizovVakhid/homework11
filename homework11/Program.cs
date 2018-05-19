57490cff53bc7f38529507a3837b384b199fa004
AbdulazizovVakhid <38040071+AbdulazizovVakhid@users.noreply.github.com>
2018-05-19 17:13:47 +0300

    все так ?

---
+++ homework11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace homework11
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           foreach (var day in DateTime.Now.TakeData(10))
+            {
+                Console.WriteLine(day);
+            }
+            Console.ReadKey();
+        }
+    }
+    static class TakeData1
+    {
+        public static IEnumerable<DateTime> TakeData(this DateTime date, int d)
+        {
+            //DateTime yesterday = DateTime.Now;
+            //DateTime then = yesterday.AddDays(double.Parse(Console.ReadLine()));
+            for (int i = 0; i < d; i++)
+            {
+                Thread.Sleep(100);
+                yield return date.AddDays(i);
+            }
+
+        }
+    }
+}
